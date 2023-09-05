0c62a6f253efe9837c830a0c516c4900f87ecb98
ktrue <webmaster@saratoga-weather.org>
2023-09-05 09:21:25 -0700

    add missing file to distribution

---
+++ NWStimeZones.php
@@ -0,0 +1,23 @@
+<?php
+$NWStimeZones = array (
+  'A' => 'America/Anchorage', // alaska
+  'Ah' => 'America/Anchorage', // alaska islands
+  'C' => 'America/Chicago',   // central
+  'CE' => 'America/Chicago',  // florida west
+  'CM' => 'America/Chicago',  // north dakota
+  'E' => 'America/New_York',  // eastern
+  'F' => 'Pacific/Fiji',      // Fiji and Yap
+  'G' => 'Pacific/Guam',      // guam and marianas
+  'H' => 'Pacific/Honolulu',  // hawaii - no DST
+  'J' => 'Asia/Tokyo',        // japan
+  'K' => 'Pacific/Kwajalein', // Marshall islands
+  'M' => 'America/Denver',    // mountain
+  'MC' => 'America/Denver',    // nebraska
+  'MP' => 'America/Los_Angeles', // idaho - western
+  'Mm' => 'America/Denver',    // arizona/reservations with DST
+  'P' => 'America/Los_Angeles',// pacific
+  'S' => 'Pacific/Pago_Pago', // samoa
+  'V' => 'America/St_Thomas', // Puerto Rico/St Thomas etc.
+  'h' => 'America/Adak',      // hawaii with DST observed
+  'm' => 'America/Phoenix',   // mountain-no DST
+);
